12b75188dd2f0a6fe78857b966fa35e9163feafa
Daniel Lira <daniel.liras87@gmail.com>
2023-12-13 15:27:04 -0500

    adds api folder and routes

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server.js";
+import { prisma } from "@/app/lib/prisma.js";
+
+export async function GET() {
+  const posts = await prisma.post.findMany();
+  return NextResponse.json({ success: true, posts });
+}
+
+export async function POST(request, response) {
+  try {
+    const { text } = await request.json();
+    if (!text) {
+      return NextResponse.json({
+        success: false,
+        error: "You must enter a valid message",
+      });
+    }
+    const post = await prisma.post.create({ data: { text } });
+    return NextResponse.json({
+      success: true,
+      post,
+    });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}
