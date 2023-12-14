f6923ea4a19e96c73cf9af2f02d03c99a01d5f6e
Daniel Lira <daniel.liras87@gmail.com>
2023-12-14 14:04:20 -0500

    updated post comment with a try catch

--- src/app/api/posts/[postId]/comments/route.js
+++ src/app/api/posts/[postId]/comments/route.js
@@ -2,48 +2,63 @@ import { NextResponse } from "next/server.js";
 import { prisma } from "@/app/lib/prisma.js";
 
 export async function GET(request, response) {
-  const { postId } = response.params;
+  try {
+    const { postId } = response.params;
 
-  const foundPost = await prisma.post.findFirst({
-    where: {
-      id: postId,
-    },
-  });
-
-  if (!foundPost) {
-    return NextResponse.json({
-      success: false,
-      message: "No post found using that ID",
+    const foundPost = await prisma.post.findFirst({
+      where: {
+        id: postId,
+      },
     });
+
+    if (!foundPost) {
+      return NextResponse.json({
+        success: false,
+        message: "No post found using that ID",
+      });
+    }
+
+    const comments = await prisma.comment.findMany({
+      where: {
+        postId,
+      },
+    });
+
+    return NextResponse.json({ success: true, comments });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
   }
-
-  const comments = await prisma.comment.findMany({
-    where: {
-      postId,
-    },
-  });
-
-  return NextResponse.json({ success: true, comments });
 }
 
 export async function POST(request, response) {
-  const { postId } = response.params;
-  const { text } = await request.json();
+  try {
+    const { postId } = response.params;
+    const { text } = await request.json();
 
-  const foundPost = await prisma.post.findFirst({
-    where: {
-      id: postId,
-    },
-  });
-
-  if (!foundPost) {
-    return NextResponse.json({
-      success: false,
-      message: "No post found using that ID",
+    const foundPost = await prisma.post.findFirst({
+      where: {
+        id: postId,
+      },
     });
+
+    if (!foundPost) {
+      return NextResponse.json({
+        success: false,
+        message: "No post found using that ID",
+      });
+    }
+
+    if (!text) {
+      return NextResponse.json({
+        success: false,
+        message: "Please provide Text for a comment",
+      });
+    }
+
+    const comment = await prisma.comment.create({ data: { text, postId } });
+
+    return NextResponse.json({ success: true, comment });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
   }
-
-  const comment = await prisma.comment.create({ data: { text, postId } });
-
-  return NextResponse.json({ success: true, comment });
 }
